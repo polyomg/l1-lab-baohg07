d18eb147097e6c71d45bc1af249465991ce44325
baohg07 <baohg82@gmail.com>
2025-10-19 14:27:37 +0700

    Hà Gia Bảo TV00100 Lab 05

--- Lab5/src/main/java/poly/edu/Lab5Java5Application.java
+++ Lab5/src/main/java/poly/edu/Lab5Java5Application.java
@@ -1,13 +1,13 @@
-package com.poly.lab5;
+package poly.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab5Application {
+public class Lab5Java5Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lab5Application.class, args);
+        SpringApplication.run(Lab5Java5Application.class, args);
     }
 
 }
